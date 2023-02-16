b323725cdd1354d45aff4955fc084abd22a71e6b
Jaechan <dlwocks31@gmail.com>
2023-02-16 19:03:46 +0900

    detect architecture in caddy installation

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #/bin/bash
 
 caddyVersion=2.1.1
+caddyArch=$(uname -m | grep -q 'x86_64' && echo 'amd64' || echo 'arm64')
 
 echo Download Caddy
-caddyGz=caddy_${caddyVersion}_linux_amd64.tar.gz
+caddyGz=caddy_${caddyVersion}_linux_${caddyArch}.tar.gz
 curl -s -O -L https://github.com/caddyserver/caddy/releases/download/v${caddyVersion}/${caddyGz}
 tar xf ${caddyGz}
 
